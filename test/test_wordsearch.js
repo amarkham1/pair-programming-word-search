9573280859a54c1bf6006645303311ab5ff23b69
amarkham1 <andrewjmarkham1@gmail.com>
2020-09-24 02:18:12 +0000

    working version

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -4,21 +4,37 @@ const assert = chai.assert;
 const wordSearch = require('../wordsearch.js')
 
 describe("#wordSearch()", function() {
-  it("should return false if the word is not present", function() {
+  it("should return true if the word is present diagonally", function() {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
       ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
-      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
-      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['H', 'M', 'R', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'A', 'Y', 'E', 'R', 'L'],
       ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
-      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['U', 'B', 'T', 'W', 'A', 'K', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+     ], 'FRANK')
+
+     assert.isTrue(result);
+   });
+  it("should return true if the word is to the left", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'W', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'D', 'E', 'L', 'D'],
+      ['Y', 'D', 'C', 'D', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'K', 'N', 'A', 'R', 'F', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'D', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'D', 'Q', 'U', 'A', 'L'],
     ], 'FRANK')
 
-    assert.isFalse(result);
+    assert.isTrue(result);
   });
+
 
   it("should return true if the word is present", function() {
     const result = wordSearch([
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,75 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+    return horizontalSearch(letters, word) || verticalSearch(letters, word) || diagonalSearch(letters, word);
+};
+
+const horizontalSearch = (matrix, word) => {
+    for (let row of matrix) {
+        if(rowSearch(row.join(''), word) || rowSearch(row.reverse().join(''), word))
+            return true;
     }
+};
+
+const rowSearch = (str, word) => str.match(word);
+
+const verticalSearch = (matrix, word) => {
+    let transposed = transpose(matrix);
+    return horizontalSearch(transposed, word);
+}
+const transpose = (array) => {
+    let temp = [];
+    let maxLen = 0;
+    maxLen = array.reduce((max, arr) => Math.max(arr.length, max), 0);
+    for (let i = 0; i < maxLen; i++) //initialize array size
+        temp.push([]);
+    for (let i = 0; i < array.length; i++) { //Grab reverse position from input matrix
+        for (let j = 0; j < array[i].length; j++) {
+            temp[j][i] = array[i][j];
+        }
+    }
+    //replaces undefined values with empty character ' ' 
+    for (let i = 0; i < temp.length; i++) {
+        for (let j = 0; j < temp[i].length; j++)
+            if (temp[i][j] === undefined)
+                temp[i][j] = ' ';
+    }
+    return temp;
+}
+
+const reverse = matrix => {
+    let reversed = [];
+    for (let row of matrix) {
+        reversed.push(row.reverse());
+    }
+    return reversed;
+}
+
+const diagonalSearch = (matrix, word) => {
+    let transposed = transpose(matrix);
+    let reversed = reverse(matrix);
+    let transposedReversed = reverse(transpose(matrix));
+
+    return searchDiagonal(matrix, word) || searchDiagonal(reversed, word) || searchDiagonal(transposed, word) || searchDiagonal(transposedReversed, word);
+}
+
+const searchDiagonal = (matrix, word) => {
+    for (let i = 0; i < matrix.length; i++) {
+        for (let j = 0; j < matrix[i].length; j++) {
+            if (matrix[i][j] === word[0]) {
+                let row = i + 1;
+                let col = j + 1;
+                let wordIndex = 1;
+                while (matrix[row] && matrix[row][col] && matrix[row][col] === word[wordIndex] && wordIndex < word.length) {
+                    row++;
+                    col++;
+                    wordIndex++;
+                }
+                if (wordIndex === word.length)
+                    return true;
+                wordIndex = 1;
+            }
+        }
+    }
+    return false;
 }
 
 module.exports = wordSearch
